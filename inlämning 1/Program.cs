6836d74749c94f8aad6aee9dd9a34f24bcad3b90
vhvalbone <albin06svensson@gmail.com>
2024-02-02 12:29:39 +0100

    Add project files.

---
+++ inlämning 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Uppgift_9._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Här får du skriva in valfritt antal namn, avsluta med tom rad");
+
+            List<string> namnLista = new List<string>();
+
+            string nyttNamn = " ";
+            while (nyttNamn != "")
+            {
+                Console.WriteLine("Skriv in ett namn");
+                nyttNamn = Console.ReadLine();
+                if (nyttNamn != "")
+                {
+                    namnLista.Add(nyttNamn);
+                }
+            }
+
+            namnLista.Sort();
+
+            Console.WriteLine("Här är namnen i omvänd bokstavsordning");
+            for (int i = namnLista.Count - 1; i >= 0; i--)
+            {
+                Console.WriteLine(namnLista[i]);
+            }
+
+        }
+    }
+}
